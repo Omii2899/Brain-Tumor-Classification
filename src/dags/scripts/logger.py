17b91b37aa8563a816290bfe9eff9a99c4c9af67
p101010 <praneithranganath10@gmail.com>
2024-06-09 19:33:15 -0400

    Logger issue fixed

--- src/dags/scripts/logger.py
+++ src/dags/scripts/logger.py
@@ -1,12 +1,20 @@
 import logging
+from datetime import datetime
 
 def setup_logging():
     
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)  # Set level to INFO to capture both info and error messages
 
+    # Get the current date and time
+    now = datetime.now()
+
+    # Format the datetime object to a string
+    timestamp_str = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    log_file_name = 'log'+timestamp_str
     # Create handler for combined logging
-    combined_handler = logging.FileHandler('./dags/logs.log')
+    combined_handler = logging.FileHandler('./dags/'+log_file_name+'.log')
     combined_handler.setLevel(logging.INFO)  # Capture all logs above INFO level
 
     # Create formatter and add it to the handler
