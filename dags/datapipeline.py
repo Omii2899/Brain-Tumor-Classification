df3ccb99d53f941b5d213ec3c930fc566b50f487
akshi-singh <akshita2000singh@gmail.com>
2024-06-09 12:58:50 -0400

    Intial changes in config file

--- dags/datapipeline.py
+++ dags/datapipeline.py
@@ -1,5 +1,7 @@
-import airflow
-
+from datetime import datetime, timedelta  
+from airflow import DAG  
+from airflow.operators.dummy_operator import DummyOperator  
+from airflow.operators.python_operator import PythonOperator
 
 with DAG("forex_data_pipeline1", start_date = datetime(2024,6,1), 
      schedule_interval = "@daily", default_args = default_args, catchup = False) as dag:
