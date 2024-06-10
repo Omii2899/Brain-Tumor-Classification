83625270d394a8bcb8e9f02d7b0732ccf9bc9f48
akshi-singh <akshita2000singh@gmail.com>
2024-06-10 12:58:32 -0400

    data/pipeline changes

--- dags/datapipeline.py
+++ dags/datapipeline.py
@@ -6,4 +6,4 @@ from airflow.operators.python_operator import PythonOperator
 with DAG("forex_data_pipeline1", start_date = datetime(2024,6,1), 
      schedule_interval = "@daily", default_args = default_args, catchup = False) as dag:
 
-    check_data_source = HttpSensor
+    check_data_source = HttpSensorgi
