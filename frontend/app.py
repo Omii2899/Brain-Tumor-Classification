72484096bb74c1e191172b928010ec5540087c3e
Aadarsh Siddha <siddha.a@northeastern.edu>
2024-06-20 19:58:21 -0400

    Moved frontend.py to frontend

--- frontend/app.py
+++ frontend/app.py
@@ -3,18 +3,22 @@ import requests
 from pathlib import Path
 from streamlit.logger import get_logger
 from PIL import Image
+import io
+import base64
 
-FASTAPI_BACKEND_ENDPOINT = "http://placeholder"
+
+FASTAPI_BACKEND_ENDPOINT = "http://localhost:8000"
 
 # Streamlit logger
 LOGGER = get_logger(__name__)
 
 # Streamlit App
 def main():
+
     # Set the main dashboard page browser tab title and icon
     st.set_page_config(
         page_title="Brain Tumor Classification",
-        page_icon=":brain:",
+        page_icon="🧠",
     )
 
     # Build the sidebar first
@@ -23,15 +27,15 @@ def main():
         try:
             backend_request = requests.get(FASTAPI_BACKEND_ENDPOINT)
             if backend_request.status_code == 200:
-                st.success("Backend online :white_check_mark:")
+                st.success("Backend online ✅")
             else:
-                st.warning("Problem connecting :sob:")
+                st.warning("Problem connecting 😭")
         except requests.ConnectionError as ce:
             LOGGER.error(ce)
-            LOGGER.error("Backend offline :scream:")
-            st.error("Backend offline :scream:")
+            LOGGER.error("Backend offline 😱")
+            st.error("Backend offline 😱")
 
-    st.write("# Brain Tumor Classification! :brain:")
+    st.write("# Brain Tumor Classification! 🧠")
     st.write("""
     ## Introduction
     This MLOps project aims to classify brain tumors using MRI images. 
@@ -39,7 +43,7 @@ def main():
     """)
 
     # Image upload section
-    uploaded_image = st.file_uploader("Upload a Brain MRI Image", type=["jpg", "jpeg", "png"])
+    uploaded_image = st.file_uploader("Upload a Brain MRI Image", type=["jpg", "jpeg"])
 
     # Check if client has provided an input image file
     if uploaded_image:
@@ -54,15 +58,46 @@ def main():
     predict_button = st.button('Predict')
 
     if predict_button and uploaded_image:
+        # Convert image to JPEG format in memory
+        image_buffer = io.BytesIO()
+        image.save(image_buffer, format='JPEG')
+        image_buffer.seek(0)
+
         # Send the image to the FastAPI server for prediction
-        files = {"file": uploaded_image.getvalue()}
+        files = {"file": ("image.jpg", image_buffer, "image/jpeg")}
         response = requests.post(f"{FASTAPI_BACKEND_ENDPOINT}/predict/", files=files)
 
         if response.status_code == 200:
-            prediction = response.json().get("prediction")
+            result = response.json()
+            prediction = result['Prediction']
+
+            # Decode the base64 images
+            inference_image = Image.open(io.BytesIO(base64.b64decode(result['Inference'])))
+            boundaries_image = Image.open(io.BytesIO(base64.b64decode(result['Boundaries'])))
             st.write(f"Prediction: {prediction}")
+
+            # Display images side by side
+            col1, col2 = st.columns(2)
+            with col1:
+                st.image(inference_image, caption='Explanation', use_column_width=False, width=300)
+            with col2:
+                st.image(boundaries_image, caption='Marked Boundaries', use_column_width=False, width=300)
+
+        elif response.status_code == 400:
+            result = response.json()
+            st.error(result['error'])
+            # st.write('Preview Image')
+            # st.image(image, caption='Uploaded Image', use_column_width=False, width=300)
+            retry_button = st.button('Retry')
+            if retry_button:
+                st.session_state["IS_IMAGE_FILE_AVAILABLE"] = False  # Reset image availability flag
+                st.experimental_rerun()  # Rerun the app to allow re-uploading
+            #retry_button = st.button('Retry')
+            
         else:
             st.write("Error: Could not get a prediction.")
 
+    if not uploaded_image or (uploaded_image and not st.session_state["IS_IMAGE_FILE_AVAILABLE"]):
+        st.info("Please upload an image to proceed.")
 if __name__ == "__main__":
     main()
